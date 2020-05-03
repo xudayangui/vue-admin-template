3f4f9e3c0ce9eb6de0a8ec4cbcdb4784ea4e0162
dayangui <dayangui@dayanguideMacBook-Pro.local>
2020-05-03 23:18:21 +0800

    buf

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -12,7 +12,7 @@
 			<template slot="title">
 				<item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
 			</template>
-			<sidebar-itemv-for="child in item.children" :key="child.path" :is-nest="true" :item="child" :base-path="resolvePath(child.path)" class="nest-menu"/>
+			<sidebar-item v-for="child in item.children" :key="child.path" :is-nest="true" :item="child" :base-path="resolvePath(child.path)" class="nest-menu"/>
 		</el-submenu>
     </div>
 </template>
