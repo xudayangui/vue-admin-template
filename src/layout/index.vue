5d177c3e37d52b0d584e6f0f2658a9b3911044aa
dayangui <dayangui@dayanguideMacBook-Pro.local>
2020-05-05 20:50:02 +0800

    bug

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div :class="classObj" class="app-wrapper">
-        <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
-            <sidebar class="sidebar-container" />
-        <div class="main-container">
-            <div :class="{'fixed-header':fixedHeader}">
-                <navbar />
-            </div>
-            <app-main />
-        </div>
-    </div>
+	<div :class="classObj" class="app-wrapper">
+		<div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+			<sidebar class="sidebar-container" />
+		<div class="main-container">
+			<div :class="{'fixed-header':fixedHeader}">
+				<navbar />
+			</div>
+			<app-main />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -18,9 +18,9 @@ import ResizeMixin from './mixin/ResizeHandler'
 export default {
     name: 'Layout',
     components: {
-            Navbar,
-            Sidebar,
-            AppMain
+		Navbar,
+		Sidebar,
+		AppMain
     },
     mixins: [ResizeMixin],
     computed: {
@@ -54,40 +54,40 @@ export default {
   @import "~@/styles/mixin.scss";
   @import "~@/styles/variables.scss";
 
-  .app-wrapper {
-    @include clearfix;
-    position: relative;
-    height: 100%;
-    width: 100%;
-    &.mobile.openSidebar{
-      position: fixed;
-      top: 0;
-    }
-  }
-  .drawer-bg {
-    background: #000;
-    opacity: 0.3;
-    width: 100%;
-    top: 0;
-    height: 100%;
-    position: absolute;
-    z-index: 999;
-  }
+  	.app-wrapper {
+		@include clearfix;
+		position: relative;
+		height: 100%;
+		width: 100%;
+		&.mobile.openSidebar{
+			position: fixed;
+			top: 0;
+		}
+  	}
+	.drawer-bg {
+		background: #000;
+		opacity: 0.3;
+		width: 100%;
+		top: 0;
+		height: 100%;
+		position: absolute;
+		z-index: 999;
+	}
 
-  .fixed-header {
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 9;
-    width: calc(100% - #{$sideBarWidth});
-    transition: width 0.28s;
-  }
+	.fixed-header {
+		position: fixed;
+		top: 0;
+		right: 0;
+		z-index: 9;
+		width: calc(100% - #{$sideBarWidth});
+		transition: width 0.28s;
+	}
 
-  .hideSidebar .fixed-header {
-    width: calc(100% - 54px)
-  }
+	.hideSidebar .fixed-header {
+		width: calc(100% - 54px)
+	}
 
-  .mobile .fixed-header {
-    width: 100%;
-  }
+	.mobile .fixed-header {
+		width: 100%;
+	}
 </style>
