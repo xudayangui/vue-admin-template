a41cb7f1636c7fab052ed8f7b3bbe4564e3debd0
len <xudayangui@gmail.com>
2020-05-16 21:06:55 +0800

    bug

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,4 @@
 import request from '@/utils/request'
-export function login(data) {return request({url: '/login',method: 'get',data})}
+export function login(params) {return request({url: '/login',method: 'get',params})}
 export function getInfo(token) {return request({url: '/getInfo',method: 'get',params: { token }})}
 export function logout(token) {return request({url: '/logout',method: 'get',params: { token }})}
