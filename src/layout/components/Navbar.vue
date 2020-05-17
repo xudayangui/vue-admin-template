2b5af30ac0dcc1c34be8d40dd541293591d7190a
len <xudayangui@gmail.com>
2020-05-17 23:27:34 +0800

    bug

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -3,13 +3,29 @@
 		<hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
 		<breadcrumb class="breadcrumb-container" />
 		<div class="right-menu">
-			<span style="font-size:15px;padding-right:20px;">
-				{{name}}
-			</span>
-			<span style="cursor:pointer;font-size:15px;" @click="logout">
-				<i class="el-icon-switch-button" ></i>
-				退出
-			</span>
+			<el-badge is-dot @click.native="toggleMsgShow" style="margin-right:10px;">
+              	<i class="el-icon-message-solid"></i>
+            </el-badge>
+			<el-dropdown trigger="click">
+				<span class="el-dropdown-link">
+					{{name}}
+					<i class="el-icon-arrow-down el-icon--right"></i>
+				</span>
+				<el-dropdown-menu slot="dropdown">
+					<el-dropdown-item>github</el-dropdown-item>
+					<el-dropdown-item divided @click.native="logout">退出</el-dropdown-item>
+				</el-dropdown-menu>
+			</el-dropdown>
+			<el-drawer  title="消息中心" size="18%" :visible.sync="drawer"  >
+				<ul class="conUl">
+					<li v-for="item in msgList" :key="item.id">
+					<router-link :to="item.link" class="conUl_link">
+						<el-link class="conUl_sp0" :underline="false">{{ item.content }}</el-link>
+						<span class="conUl_sp1">{{ item.time }}</span>
+					</router-link>
+					</li>
+				</ul>
+			</el-drawer>
 		</div>
 	</div>
 </template>
@@ -23,6 +39,37 @@ import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 
 export default {
+	data() {
+		return {
+			drawer: false,
+			msgList: [
+				{
+					id: '1',
+					content: '优惠券到期提醒',
+					link: '',
+					time: '2019-06-01'
+				},
+				{
+					id: '2',
+					content: '618大促，请查看活动具体信息',
+					link: '',
+					time: '2019-06-02'
+				},
+				{
+					id: '3',
+					content: '充值成功',
+					link: '',
+					time: '2019-07-02'
+				},
+				{
+					id: '4',
+					content: '密码充值成功！',
+					link: '',
+					time: '2019-07-02'
+				}
+			]
+		};
+    },
 	components: {
 		Breadcrumb,
 		Hamburger
@@ -33,6 +80,9 @@ export default {
 		])
 	},
 	methods: {
+		toggleMsgShow(){
+			this.drawer = true
+		},
 		//伸缩
 		toggleSideBar() {
 			this.$store.dispatch('app/toggleSideBar')
@@ -55,6 +105,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+}
+.el-icon-arrow-down {
+    font-size: 12px;
+}
+.conUl {
+	li {
+		height: 50px;
+		line-height: 50px;
+		list-style-type:none;
+		.conUl_link {
+			display: block;
+			height: 100%;
+			margin: 0 20px 0 0;
+			border-bottom: 1px solid #e4e4e4;
+		}
+		.conUl_sp0 {
+			font-size: 14px;
+		}
+		.conUl_sp1 {
+			font-size: 12px;
+			color: #b3b3b3;
+			float: right;
+		}
+	}
+}
 .navbar {
 	height: 50px;
 	overflow: hidden;
@@ -88,6 +166,10 @@ export default {
 		&:focus {
 			outline: none;
 		}
+	}
+	.el-badge {
+		cursor: pointer;
+		
 	}
 }
 </style>
