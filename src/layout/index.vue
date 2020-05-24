28a3d3e10fcc2d693722101ab18df6b1a757b335
len <xudayangui@gmail.com>
2020-05-24 18:15:13 +0800

    优化

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -13,8 +13,6 @@
 
 <script>
 import { Navbar, Sidebar, AppMain } from './components'
-import ResizeMixin from './mixin/ResizeHandler'
-
 export default {
     name: 'Layout',
     components: {
@@ -22,7 +20,6 @@ export default {
 		Sidebar,
 		AppMain
     },
-    mixins: [ResizeMixin],
     computed: {
         sidebar() {
             return this.$store.state.app.sidebar
