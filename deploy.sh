9b18ef0b6c47bb921a40f2507acb85722cce4062
len <xudayangui@gmail.com>
2020-11-26 20:11:46 +0400

    bug

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# deploy.sh
+# 错误时停止
+set -e
+# 打包
+npm run build
+# 进入目标文件夹
+cd dist
+# 提交到本地仓库
+git init
+git add -A
+git commit -m 'deploy'
+# 提交到 https://github.com:nusr/resume-vue 项目的 gh-pages 分支
+git push -f https://github.com/xudayangui/vue-admin-template.git master:gh-pages
+cd -
