96e38711482bf5024af5c86a4c042135bd14c058
len <xudayangui@gmail.com>
2020-11-30 22:01:15 +0400

    deploy

---
+++ static/js/chunk-2d0ba4ad.a945544c.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba4ad"],{"371d":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},r=[],a=t("2877"),c={},l=Object(a["a"])(c,u,r,!1,null,null,null);n["default"]=l.exports}}]);
